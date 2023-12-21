4246845fe7849d6fc48e4d4a32712985df4d1b99
Mahmoud Elzain <73619807+engzain333@users.noreply.github.com>
2023-12-21 21:29:52 +0200

    Add files via upload

---
+++ 1-MCAL/05-UART/USART_config.h
@@ -0,0 +1,22 @@
+/*
+              ################################   UASRT_config.h   ######################
+			  
+ *   Author : Mahmoud Zain
+ 
+ *   Layer  : MCAL
+ 
+ *   SWC    : USART
+	 
+ *   date   : 20/11/2023
+ */
+ 
+ 
+ 
+ 
+ #ifndef   UASRT_CONFIG_H_
+ #define   UASRT_CONFIG_H_
+ 
+ 
+ 
+ 
+ #endif    /*   USART_CONFIG_H_  */  
